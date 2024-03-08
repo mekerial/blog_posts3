3b4f2ce7cf4c2dba76d124a182bf574ed3367868
MEKERIAL <mekery00@gmail.com>
2024-03-08 18:27:38 +0300

    add checking for already confirmed emails

--- src/middlewares/auth/email-confirmation-middleware.ts
+++ src/middlewares/auth/email-confirmation-middleware.ts
@@ -4,8 +4,11 @@ import {inputValidation} from "../input-model-validation/input-validation";
 
 export const isConfirmedValidation = body('code').isString().trim().custom(async (code) => {
     const user = await UserRepository.getUserByVerifyCode(code)
+    if (!user) {
+        throw Error("not found!")
+    }
 
-    if (user) {
+    if (user.emailConfirmation.isConfirmed) {
         throw Error("email is already confirm!")
     }
     return true
