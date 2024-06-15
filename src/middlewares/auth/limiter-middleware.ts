a5e117ec3dcdba805159906750bb3b378219003c
MEKERIAL <mekery00@gmail.com>
2024-06-15 13:22:22 +0300

    change limit requests in limiter on 5 reqeusts

--- src/middlewares/auth/limiter-middleware.ts
+++ src/middlewares/auth/limiter-middleware.ts
@@ -2,7 +2,7 @@ import rateLimit from 'express-rate-limit';
 
 export const limiter = rateLimit({
     windowMs: 10 * 1000, // 10 секунд
-    max: 4, // максимум 5 запросов с одного IP
+    max: 5, // максимум 5 запросов с одного IP
     message: 'Many requests, try again after 10 seconds',
     statusCode: 429, // статус ответа при превышении лимита
 });
