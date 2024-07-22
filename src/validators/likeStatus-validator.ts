0c217a5f56db53859a11642e3e11c8c5eed274f1
MEKERIAL <mekery00@gmail.com>
2024-07-22 18:49:39 +0500

    microbug

--- src/validators/likeStatus-validator.ts
+++ src/validators/likeStatus-validator.ts
@@ -1,6 +1,6 @@
 import {body} from 'express-validator'
 import {inputValidation} from "../middlewares/input-model-validation/input-validation";
 
-export const likeStatusValidate = body('content').isString().trim().isIn([ "None", "Like", "Dislike" ]).withMessage('Incorrect content!')
+export const likeStatusValidate = body('likeStatus').isString().trim().isIn([ "None", "Like", "Dislike" ]).withMessage('Incorrect content!')
 
 export const likeStatusValidation = () => [likeStatusValidate, inputValidation]
